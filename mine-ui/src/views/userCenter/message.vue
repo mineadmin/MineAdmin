1639c403baef36a59e5209812de2ecc3fe1f449d
X.Mo <root@imoi.cn>
2022-04-01 16:44:17 +0800

    up

--- mine-ui/src/views/userCenter/message.vue
+++ mine-ui/src/views/userCenter/message.vue
@@ -101,7 +101,7 @@
 
           <el-table-column
             label="操作"
-            width="150"
+            width="220"
           >
             <template #default="scope">
               <el-button type="text" @click="del(scope.row.id)">删除</el-button>

1751300ae214e52a087280f92a2ec804af9ec237
X.Mo <root@imoi.cn>
2022-07-06 10:41:32 +0800

    up

--- app/System/Model/SystemUser.php
+++ app/System/Model/SystemUser.php
@@ -57,7 +57,7 @@ class SystemUser extends MineModel
      *
      * @var array
      */
-    protected $casts = ['id' => 'integer', 'dept_id' => 'integer', 'status' => 'integer', 'created_by' => 'integer', 'updated_by' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+    protected $casts = ['id' => 'integer', 'dept_id' => 'integer', 'status' => 'integer', 'created_by' => 'integer', 'updated_by' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'backend_setting' => 'array'];
     /**
      * 通过中间表关联角色
      * @return \Hyperf\Database\Model\Relations\BelongsToMany

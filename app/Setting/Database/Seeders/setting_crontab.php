c3ee68377bb0f2738061c6f8f738e9431c8ecdf9
X.Mo <root@imoi.cn>
2022-07-26 20:40:45 +0800

    up

--- app/Setting/Database/Seeders/setting_crontab.php
+++ app/Setting/Database/Seeders/setting_crontab.php
@@ -33,8 +33,8 @@ class SettingCrontab extends Seeder
     {
         $tableName = env('DB_PREFIX').\App\Setting\Model\SettingCrontab::getModel()->getTable();
         return [
-            "INSERT INTO `{$tableName}` VALUES (1, 'urlCrontab', '3', 'http://127.0.0.1:9501/', '', '59 */1 * * * *', '1', 2, NULL, NULL, '2021-08-07 23:28:28', '2021-08-07 23:44:55', '请求127.0.0.1')",
-            "INSERT INTO `{$tableName}` VALUES (2, '每月1号清理所有日志', '2', 'App\System\Crontab\ClearLogCrontab', '', '0 0 0 1 * *', '1', 2, NULL, NULL, '2022-04-11 11:15:48', '2022-04-11 11:15:48', '')"
+            "INSERT INTO `{$tableName}` VALUES (1, 'urlCrontab', '3', 'http://127.0.0.1:9501/', '', '59 */1 * * * *', 2, 2, NULL, NULL, '2021-08-07 23:28:28', '2021-08-07 23:44:55', '请求127.0.0.1')",
+            "INSERT INTO `{$tableName}` VALUES (2, '每月1号清理所有日志', '2', 'App\System\Crontab\ClearLogCrontab', '', '0 0 0 1 * *', 2, 2, NULL, NULL, '2022-04-11 11:15:48', '2022-04-11 11:15:48', '')"
         ];
     }
 }

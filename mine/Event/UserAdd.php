c68a7e4ccd94ea049286f77d6433a1e4307d9b57
X.Mo <root@imoi.cn>
2022-10-13 14:00:48 +0800

    feat: 新增用户添加和删除事件

---
+++ mine/Event/UserAdd.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * MineAdmin is committed to providing solutions for quickly building web applications
+ * Please view the LICENSE file that was distributed with this source code,
+ * For the full copyright and license information.
+ * Thank you very much for using MineAdmin.
+ *
+ * @Author X.Mo<root@imoi.cn>
+ * @Link   https://gitee.com/xmo/MineAdmin
+ */
+
+declare(strict_types = 1);
+namespace Mine\Event;
+
+class UserAdd
+{
+    public array $userinfo;
+
+    public function __construct(array $userinfo)
+    {
+        $this->userinfo = $userinfo;
+    }
+}

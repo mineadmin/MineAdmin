06870c13ba16efb4b67a76165b5ac72fa8da0517
X.Mo <root@imoi.cn>
2023-04-21 16:49:09 +0800

    feat: 增加重启服务脚本

---
+++ bin/reboot.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MineAdmin is committed to providing solutions for quickly building web applications
+ * Please view the LICENSE file that was distributed with this source code,
+ * For the full copyright and license information.
+ * Thank you very much for using MineAdmin.
+ *
+ * @Author X.Mo<root@imoi.cn>
+ * @Link   https://gitee.com/xmo/MineAdmin
+ */
+
+$pid = shell_exec(sprintf('cat %s/../runtime/hyperf.pid', __DIR__));
+$rebootCmd = sprintf('rm -rf %s/../runtime/container/* && php %s/hyperf.php start > /dev/null 2>/dev/null &', __DIR__, __DIR__);
+
+if (shell_exec(sprintf('ps -ef | grep -v grep | grep %s', $pid))) {
+    shell_exec("kill -9 {$pid}");
+    shell_exec($rebootCmd);
+} else {
+    shell_exec($rebootCmd);
+}

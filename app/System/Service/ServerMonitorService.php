547c1c0aea350d1db27f980ef82cab455b3f5ceb
netyum <wayhood@163.com>
2023-05-07 23:34:10 +0800

    fix php version info

--- app/System/Service/ServerMonitorService.php
+++ app/System/Service/ServerMonitorService.php
@@ -155,7 +155,8 @@ class ServerMonitorService
      */
     public function getPhpAndEnvInfo(): array
     {
-        preg_match('/(\d\.\d\.\d)/', shell_exec('php --ri swoole | grep Version'), $matches);
+        $interpreter = $_SERVER['_'];
+        preg_match('/(\d\.\d\.\d)/', shell_exec("$interpreter --ri swoole | grep Version"), $matches);
         $result['swoole_version'] = $matches[1];
 
         $result['php_version'] = PHP_VERSION;

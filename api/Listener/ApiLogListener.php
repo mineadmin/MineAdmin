f74a1d7f85cd521f9ca1e27c1af7dc3bd21b482c
netyum <wayhood@163.com>
2023-05-08 21:12:10 +0800

    update 接口日志协程延时写入，以便快速响应

--- api/Listener/ApiLogListener.php
+++ api/Listener/ApiLogListener.php
@@ -17,6 +17,7 @@ use Hyperf\Event\Contract\ListenerInterface;
 use Mine\Event\ApiAfter;
 use Mine\Helper\Str;
 use Mine\MineRequest;
+use Hyperf\Utils\Coroutine;
 
 /**
  * API访问日志保存
@@ -44,6 +45,8 @@ class ApiLogListener implements ListenerInterface
      */
     public function process(object $event): void
     {
+
+        
         /* @var $event ApiAfter */
         $data = $event->getApiData();
         $request = container()->get(MineRequest::class);
@@ -62,18 +65,34 @@ class ApiLogListener implements ListenerInterface
             $app_id = $data['id'] ?? 0;
             $app_id = is_numeric($app_id) ? $app_id : 0;
 
-            $service->save([
-                'api_id' => $app_id,
-                'api_name' => $data['name'],
-                'access_name' => $data['access_name'],
-                'request_data' => [ 'data' => $reqData, 'params' => $request->getQueryParams() ],
-                'response_code' => $response->getStatusCode(),
-// 返回内容以免过大，暂不保存访问内容
-//                'response_data' => $response->getBody()->getContents(),
-                'ip' => $request->ip(),
-                'ip_location' => Str::ipToRegion($request->ip()),
-                'access_time' => date('Y-m-d H:i:s')
-            ]);
+            // 放协程 尽量快速响应接口， 以及延时写库 这里没试，怕协程切换拿不到数据，先把需要的都取出来
+
+            $queryParams = $request->getQueryParams();
+            $responseCode = $response->getStatusCode();
+
+            // 返回内容以免过大，暂不保存访问内容
+            // $responseData => $response->getBody()->getContents(),
+            $responseData = '';
+
+
+            $ip = $request->ip();
+            $ipLocation = Str::ipToRegion($ip);
+            $accessTime = date('Y-m-d H:i:s');
+            Coroutine::create(function() use ($service, $app_id, $data, $reqData, $queryParams, $responseCode, $responseData, $ip, $ipLocation, $accessTime) {
+                // 随机延时写入
+                sleep(rand(5, 30));
+                $service->save([
+                    'api_id' => $app_id,
+                    'api_name' => $data['name'],
+                    'access_name' => $data['access_name'],
+                    'request_data' => [ 'data' => $reqData, 'params' => $queryParams ],
+                    'response_code' => $responseCode,
+                    // 'response_data' => $responseData,
+                    'ip' => $ip,
+                    'ip_location' => $ipLocation,
+                    'access_time' => $accessTime
+                ]);
+            });
         }
     }
 }

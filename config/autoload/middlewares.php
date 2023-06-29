6fc5f01a2e2955b3b1a1818749dea4f745fc1b55
X.Mo <root@imoi.cn>
2023-06-29 10:50:01 +0800

    refactor

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  */
 return [
     'http' => [
+        \Mine\Middlewares\CheckModuleMiddleware::class,
         \Hyperf\Validation\Middleware\ValidationMiddleware::class,
-        \Mine\Middlewares\CheckModuleMiddleware::class
     ],
 ];

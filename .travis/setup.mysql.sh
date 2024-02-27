af06663c6c839124e8daef021c77425a04b09710
Death-Satan <49744633+Death-Satan@users.noreply.github.com>
2024-02-28 01:09:54 +0800

    Reset Mysql Container

--- .travis/setup.mysql.sh
+++ .travis/setup.mysql.sh
@@ -4,7 +4,7 @@ CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$(dirname $(dirname $CURRENT_DIR))}"
 
 echo -e "Create MySQL database..."
-mysql -h 127.0.0.1 -u root -proot -e "CREATE DATABASE IF NOT EXISTS mineadmin charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+mysql -h 127.0.0.1 -u root -e "CREATE DATABASE IF NOT EXISTS mineadmin charset=utf8mb4 collate=utf8mb4_unicode_ci;"
 
 echo -e "Done\n"
 
--- .travis/setup.services.sh
+++ .travis/setup.services.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=true -d mysql:${MYSQL_VERSION} --bind-address=0.0.0.0 --default-authentication-plugin=mysql_native_password &
+ docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=true -d mysql:${MYSQL_VERSION} --bind-address=0.0.0.0 --default-authentication-plugin=mysql_native_password &
 docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres:${PGSQL_VERSION} &
 docker run --name redis -p 6379:6379 -d redis &
 docker run -d --name dev-consul -e CONSUL_BIND_INTERFACE=eth0 -p 8500:8500 consul:1.15.4 &

314e6bd254d80aa8058586298200ee96bfdb8feb
Death-Satan <49744633+Death-Satan@users.noreply.github.com>
2024-02-28 01:08:13 +0800

    Set mysql root password

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -21,7 +21,7 @@ return [
         'database' => env('DB_DATABASE', 'hyperf'),
         'port' => env('DB_PORT', 3306),
         'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD'),
+        'password' => env('DB_PASSWORD','root'),
         'charset' => env('DB_CHARSET', 'utf8mb4'),
         'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),

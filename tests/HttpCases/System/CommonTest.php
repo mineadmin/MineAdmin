6d73cddaef2d28390aec87ca68c18a09caa4e505
Death-Satan <49744633+Death-Satan@users.noreply.github.com>
2024-02-29 00:26:32 +0800

    Code style

--- tests/HttpCases/System/CommonTest.php
+++ tests/HttpCases/System/CommonTest.php
@@ -1,44 +1,54 @@
 <?php
-beforeEach(function (){
+
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+beforeEach(function () {
     $this->prefix = '/system/common';
 });
 
-test('getUserList test',function (){
-    testSuccessResponse($this->get($this->prefix.'/getUserList'));
+test('getUserList test', function () {
+    testSuccessResponse($this->get($this->prefix . '/getUserList'));
 });
 
-test('getUserInfoByIds test',function (){
-    testSuccessResponse($this->post($this->prefix.'/getUserInfoByIds',['ids'=>[1,2,3]]));
+test('getUserInfoByIds test', function () {
+    testSuccessResponse($this->post($this->prefix . '/getUserInfoByIds', ['ids' => [1, 2, 3]]));
 });
 
-test('getDeptTreeList test',function (){
-    testSuccessResponse($this->get($this->prefix.'/getDeptTreeList'));
+test('getDeptTreeList test', function () {
+    testSuccessResponse($this->get($this->prefix . '/getDeptTreeList'));
 });
 
-test('getRoleList test',function (){
-    testSuccessResponse($this->get($this->prefix.'/getRoleList'));
+test('getRoleList test', function () {
+    testSuccessResponse($this->get($this->prefix . '/getRoleList'));
 });
 
-test('getPostList test',function (){
-    testSuccessResponse($this->get($this->prefix.'/getPostList'));
+test('getPostList test', function () {
+    testSuccessResponse($this->get($this->prefix . '/getPostList'));
 });
 
-test('getOperationLogList test',function (){
-    testSuccessResponse($this->get($this->prefix.'/getOperationLogList'));
+test('getOperationLogList test', function () {
+    testSuccessResponse($this->get($this->prefix . '/getOperationLogList'));
 });
 
-test('getLoginLogList test',function (){
-    testSuccessResponse($this->get($this->prefix.'/getLoginLogList'));
+test('getLoginLogList test', function () {
+    testSuccessResponse($this->get($this->prefix . '/getLoginLogList'));
 });
 
-test('getResourceList test',function (){
-    testSuccessResponse($this->get($this->prefix.'/getResourceList'));
+test('getResourceList test', function () {
+    testSuccessResponse($this->get($this->prefix . '/getResourceList'));
 });
 
-test('getNoticeList test',function (){
-    testSuccessResponse($this->get($this->prefix.'/getNoticeList'));
+test('getNoticeList test', function () {
+    testSuccessResponse($this->get($this->prefix . '/getNoticeList'));
 });
 
-test('clearAllCache test',function (){
-    testSuccessResponse($this->get($this->prefix.'/clearAllCache'));
+test('clearAllCache test', function () {
+    testSuccessResponse($this->get($this->prefix . '/clearAllCache'));
 });

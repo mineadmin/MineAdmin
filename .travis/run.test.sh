a215b8cb90cf2a7c96f2a02344dfa07a3781ea09
Death-Satan <49744633+Death-Satan@users.noreply.github.com>
2024-02-28 09:16:21 +0800

    Fixed

--- .travis/run.test.sh
+++ .travis/run.test.sh
@@ -2,8 +2,10 @@
 
 set -e
 php bin/hyperf.php migrate --path=app/Setting/Database/Migrations
+php bin/hyperf.php migrate --path=app/Setting/Database/Migrations/Update
 
 php bin/hyperf.php migrate --path=app/System/Database/Migrations
+php bin/hyperf.php migrate --path=app/System/Database/Migrations/Update
 
 php bin/hyperf.php db:seed --path=app/Setting/Database/Seeders
 

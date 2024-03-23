ee0aed109f81637f7f01360bf4a9f0477108431e
Zds <49744633+zds-s@users.noreply.github.com>
2024-03-24 04:06:40 +0800

    Update

--- tests/Feature/ModuleTest/System/Monitor/OnlineUserMonitorTest.php
+++ tests/Feature/ModuleTest/System/Monitor/OnlineUserMonitorTest.php
@@ -9,12 +9,22 @@ declare(strict_types=1);
  * @contact  root@imoi.cn
  * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
  */
+use Hyperf\Redis\Redis;
+use Hyperf\Snowflake\IdGenerator;
+
 beforeEach(function () {
     $this->prefix = '/system/onlineUser';
 });
 
 test('online user controller test', function () {
     testSuccessResponse($this->get($this->prefix . '/index'));
-    $id = $this->mock->id;
+    /**
+     * @var IdGenerator $idGenerator
+     */
+    $idGenerator = make(IdGenerator::class);
+    $redis = make(Redis::class);
+    $id = $idGenerator->generate();
+    $key = sprintf('%sToken:%s', config('cache.default.prefix'), $id);
+    $redis->set($key, '123456');
     testSuccessResponse($this->post($this->prefix . '/kick', compact('id')));
 });

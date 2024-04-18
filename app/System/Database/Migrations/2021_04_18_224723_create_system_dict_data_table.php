a728b2667cb58a9265d8a9ac5db4faff6a3c63c8
X.Mo <root@imoi.cn>
2024-04-18 15:16:30 +0800

    fix: created table migrations allow nullable

--- app/System/Database/Migrations/2021_04_18_224723_create_system_dict_data_table.php
+++ app/System/Database/Migrations/2021_04_18_224723_create_system_dict_data_table.php
@@ -25,16 +25,16 @@ class CreateSystemDictDataTable extends Migration
             $table->comment('字典数据表');
             $table->bigIncrements('id')->comment('主键');
             $table->addColumn('bigInteger', 'type_id', ['unsigned' => true, 'comment' => '字典类型ID']);
-            $table->addColumn('string', 'label', ['length' => 50, 'comment' => '字典标签'])->default('');
-            $table->addColumn('string', 'value', ['length' => 100, 'comment' => '字典值'])->default('');
-            $table->addColumn('string', 'code', ['length' => 100, 'comment' => '字典标示'])->default('');
-            $table->addColumn('smallInteger', 'sort', ['unsigned' => true, 'default' => 0, 'comment' => '排序'])->default(0);
-            $table->addColumn('smallInteger', 'status', ['default' => 1, 'comment' => '状态 (1正常 2停用)'])->default(1);
-            $table->addColumn('bigInteger', 'created_by', ['comment' => '创建者'])->default(0);
-            $table->addColumn('bigInteger', 'updated_by', ['comment' => '更新者'])->default(0);
+            $table->addColumn('string', 'label', ['length' => 50, 'comment' => '字典标签'])->nullable();
+            $table->addColumn('string', 'value', ['length' => 100, 'comment' => '字典值'])->nullable();
+            $table->addColumn('string', 'code', ['length' => 100, 'comment' => '字典标示'])->nullable();
+            $table->addColumn('smallInteger', 'sort', ['unsigned' => true, 'default' => 0, 'comment' => '排序'])->nullable();
+            $table->addColumn('smallInteger', 'status', ['default' => 1, 'comment' => '状态 (1正常 2停用)'])->nullable();
+            $table->addColumn('bigInteger', 'created_by', ['comment' => '创建者'])->nullable();
+            $table->addColumn('bigInteger', 'updated_by', ['comment' => '更新者'])->nullable();
             $table->timestamps();
             $table->addColumn('timestamp', 'deleted_at', ['precision' => 0, 'comment' => '删除时间'])->nullable();
-            $table->addColumn('string', 'remark', ['length' => 255, 'comment' => '备注'])->default('');
+            $table->addColumn('string', 'remark', ['length' => 255, 'comment' => '备注'])->nullable();
             $table->index('type_id');
         });
     }

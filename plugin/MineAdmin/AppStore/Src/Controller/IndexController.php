54085f5f7481944a0a4f3227efda7ba0d591615d
X.Mo <root@imoi.cn>
2024-04-25 16:31:28 +0800

    up

--- plugin/MineAdmin/AppStore/Src/Controller/IndexController.php
+++ plugin/MineAdmin/AppStore/Src/Controller/IndexController.php
@@ -1,12 +1,22 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+
 namespace Plugin\MineAdmin\AppStore\Controller;
 
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\GetMapping;
-use Mine\Annotation\Auth;
 use Mine\MineController;
+use Psr\Http\Message\ResponseInterface;
 use Xmo\AppStore\Service\Impl\AppStoreServiceImpl;
 
 #[Controller(prefix: 'plugin/store')]
@@ -16,9 +26,14 @@ class IndexController extends MineController
     public AppStoreServiceImpl $serviceImpl;
 
     #[GetMapping('index')]
-    public function index()
+    public function index(): ResponseInterface
     {
-        $data = $this->serviceImpl->list(['page' => 1, 'size' => 10]);
-        return $this->success($data);
+        return $this->success($this->serviceImpl->list(['page' => 1, 'size' => 10]));
+    }
+
+    #[GetMapping('hasAccessToken')]
+    public function hasAccessToken(): ResponseInterface
+    {
+        return $this->success(['isHas' => ! is_null(env('MINE_ACCESS_TOKEN'))]);
     }
 }

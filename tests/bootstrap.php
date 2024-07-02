e094254682425932c8691356636150e99b04494f
PeopleSea <70972819+People-Sea@users.noreply.github.com>
2024-07-02 22:59:01 +0800

    Fix(filterExecuteAttributes): Utilizing memory address pointers (#294)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,8 +11,8 @@ declare(strict_types=1);
  */
 use Hyperf\Contract\ApplicationInterface;
 use Hyperf\Di\ClassLoader;
-use Swoole\Runtime;
 use Mine\AppStore\Plugin;
+use Swoole\Runtime;
 
 /*
  * This file is part of MineAdmin.
@@ -30,6 +30,8 @@ date_default_timezone_set('Asia/Shanghai');
 
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 ! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
+! defined('START_TIME') && define('START_TIME', time());    // 启动时间
+! defined('HF_VERSION') && define('HF_VERSION', '3.1');     // 定义hyperf版本号
 
 // Runtime::enableCoroutine(true);
 

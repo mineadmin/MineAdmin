1bf26a33a733ca87a9d0d3dcd5f740f60fea0100
PeopleSea <PeopleSea@outlook.com>
2024-07-25 11:55:14 +0800

    feat(swagger): 新增 Swagger 配置文件

---
+++ config/autoload/swagger.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+return [
+    'enable' => true,
+    'port' => 9500,
+    'json_dir' => BASE_PATH . '/runtime/swagger',
+    'html' => null,
+    'url' => '/swagger',
+    'auto_generate' => true,
+    'scan' => [
+        'paths' => [
+            'app/Http/Admin/Controller'
+        ]
+    ],
+    'processors' => [
+        // users can append their own processors here
+    ]
+];

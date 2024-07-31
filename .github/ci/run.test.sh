5550d7aad419f287254dd923ceca34b505074f3f
PeopleSea <peoplesea@outlook.com>
2024-08-01 00:04:09 +0800

    Fix(CI): 修复生成覆盖率时可能出现的内存不足

--- .github/ci/run.test.sh
+++ .github/ci/run.test.sh
@@ -3,4 +3,5 @@
 set -e
 
 ./vendor/bin/co-phpunit --group=migrations --prepend tests/bootstrap.php --colors=always
+sleep 1
 ./vendor/bin/co-phpunit --exclude-group=migrations --prepend tests/bootstrap.php --colors=always

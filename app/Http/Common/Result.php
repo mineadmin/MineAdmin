fc8b6fcd894f5e4cd462592ec8a584b595c845ea
PeopleSea <peoplesea@outlook.com>
2024-07-31 23:30:20 +0800

    Fix: phpstan

--- app/Http/Common/Result.php
+++ app/Http/Common/Result.php
@@ -1,1 +1,50 @@
-<?phpdeclare(strict_types=1);/** * This file is part of MineAdmin. * * @link     https://www.mineadmin.com * @document https://doc.mineadmin.com * @contact  root@imoi.cn * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE */namespace App\Http\Common;use Hyperf\Contract\Arrayable;use Hyperf\Swagger\Annotation as OA;/** * @template T */#[OA\Schema(title: 'Api Response', description: 'Api Response')]class Result implements Arrayable{    /**     * @param ResultCode $code     * @param null|string $message     * @param T $data     */    public function __construct(        #[OA\Property(ref: 'ResultCode', title: '响应码')]        public ResultCode $code = ResultCode::SUCCESS,        #[OA\Property(title: '响应消息', type: 'string')]        public ?string $message = null,        #[OA\Property(title: '响应数据')]        public mixed $data = []    ) {        if ($this->message === null) {            $this->message = ResultCode::getMessage($this->code->value);        }    }    public function toArray(): array    {        return [            'code' => $this->code->value,            'message' => $this->message,            'data' => $this->data,        ];    }}
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+
+namespace App\Http\Common;
+
+use Hyperf\Contract\Arrayable;
+use Hyperf\Swagger\Annotation as OA;
+
+/**
+ * @template T
+ */
+#[OA\Schema(title: 'Api Response', description: 'Api Response')]
+class Result implements Arrayable
+{
+    /**
+     * @param ResultCode $code
+     * @param null|string $message
+     * @param T $data
+     */
+    public function __construct(
+        #[OA\Property(ref: 'ResultCode', title: '响应码')]
+        public ResultCode $code = ResultCode::SUCCESS,
+        #[OA\Property(title: '响应消息', type: 'string')]
+        public ?string $message = null,
+        #[OA\Property(title: '响应数据')]
+        public mixed $data = []
+    ) {
+        if ($this->message === null) {
+            $this->message = ResultCode::getMessage($this->code->value);
+        }
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'code' => $this->code->value,
+            'message' => $this->message,
+            'data' => $this->data,
+        ];
+    }
+}

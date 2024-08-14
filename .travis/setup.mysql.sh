516d5cfd7fe320ff46a8538e7e7da4d2e40d8631
WenRenHai <PeopleSea@outlook.com>
2024-08-14 15:03:19 +0800

    fix(ci): 修复ci测试和cs fix检查

--- .travis/setup.mysql.sh
+++ .travis/setup.mysql.sh
@@ -3,6 +3,25 @@
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$(dirname $(dirname $CURRENT_DIR))}"
 
+MAX_RETRIES=12
+RETRY_COUNT=0
+
+while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+  if mysql -h 127.0.0.1 -u root -e ";" &> /dev/null; then
+    echo -e "MySQL service is now available."
+    break
+  else
+    echo -e "Waiting for MySQL service to start..."
+    sleep 5
+    ((RETRY_COUNT++))
+  fi
+done
+
+if [ $RETRY_COUNT -eq $MAX_RETRIES ]; then
+  echo -e "MySQL service did not start within the time limit."
+  exit 1
+fi
+
 echo -e "Create MySQL database..."
 mysql -h 127.0.0.1 -u root -e "CREATE DATABASE IF NOT EXISTS mineadmin charset=utf8mb4 collate=utf8mb4_unicode_ci;"
 

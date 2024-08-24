c805c99bfa3c49bd4bc55c0b38253449ff7ab85e
zds <49744633+zds-s@users.noreply.github.com>
2024-08-24 21:03:19 +0800

    reactor(attachment): 优化后缀查询写法

--- app/Repository/AttachmentRepository.php
+++ app/Repository/AttachmentRepository.php
@@ -33,8 +33,7 @@ final class AttachmentRepository extends IRepository
     public function handleSearch(Builder $query, array $params): Builder
     {
         return $query->when(Arr::get($params, 'suffix'), function (Builder $query, $suffix) {
-            $suffix = is_array($suffix) ? $suffix : explode(',', $suffix);
-            $query->whereIn('suffix', $suffix);
+            $query->whereIn('suffix', Arr::wrap($suffix));
         });
     }
 }

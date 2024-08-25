d81301955befed1e4153343bfcc2df1e41df9f32
X.Mo <root@imoi.cn>
2024-08-25 19:31:42 +0800

    chore(优化): 🔨

--- web/src/utils/copyright.ts
+++ web/src/utils/copyright.ts
@@ -12,4 +12,4 @@ import pkg from '../../package.json'
 const copyright_common_style = 'background:#35495E; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;'
 const copyright_main_style = `background: #3488ff; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff;`
 const copyright_sub_style = `background:transparent`
-console.info(`%c MineAdmin-UI %c ${pkg.version} release %c`, copyright_common_style, copyright_main_style, copyright_sub_style, '\nhttps://github.com/mineadmin/ui')
+console.info(`%c MineAdmin %c ${pkg.version} release %c`, copyright_common_style, copyright_main_style, copyright_sub_style, '\nhttps://github.com/mineadmin')

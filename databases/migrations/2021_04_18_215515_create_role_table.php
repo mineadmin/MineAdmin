aa312ce469648fb6f220892187ad4120e2891f00
wsandos175 <1579282698@qq.com>
2024-09-19 09:27:52 +0800

    docs(迁移文件): 📝 优化注释

--- databases/migrations/2021_04_18_215515_create_role_table.php
+++ databases/migrations/2021_04_18_215515_create_role_table.php
@@ -31,7 +31,7 @@ class CreateRoleTable extends Migration
                 [
                     'length' => 1,
                     'default' => 1,
-                    'comment' => '数据范围（1：全部数据权限 2：自定义数据权限 3：本部门数据权限 4：本部门及以下数据权限 5：本人数据权限）',
+                    'comment' => '数据范围:1=全部数据权限,2=自定义数据权限,3=本部门数据权限,4=本部门及以下数据权限,5=本人数据权限',
                 ]
             )->nullable();
             $table->tinyInteger('status')->comment('状态 (1正常 2停用)')->default(1);

f03af43d68f3624f7785354eab46c6fe4226527a
wsandos175 <1579282698@qq.com>
2024-09-24 15:29:58 +0800

    重构用户组件以提高代码清晰度和可维护性
    对用户组件的代码进行了重构，移除了不必要的计算属性并简化了Avatar的图像

--- web/src/plugins/mine-admin/cell-render/components/user/index.vue
+++ web/src/plugins/mine-admin/cell-render/components/user/index.vue
@@ -1,10 +1,10 @@
 <script lang="tsx">
 import type { PropType } from 'vue'
 import { defineComponent } from 'vue'
+import { ElAvatar, ElLink, ElPopover } from 'element-plus'
 import { cellRenderPluginName, createOptions, createRowFieldValues, getConfig } from '../../utils/tools.ts'
-import {ElLink,ElPopover,ElRow,ElCol,ElAvatar} from "element-plus";
 import UserInfo from './components/userInfo.vue'
-import defaultAvatar from "@/assets/images/defaultAvatar.jpg";
+import defaultAvatar from '@/assets/images/defaultAvatar.jpg'
 
 // 定义options类型,与ImageProps类型合并
 export interface Options {
@@ -13,11 +13,6 @@ export interface Options {
 
 export default defineComponent({
   name: cellRenderPluginName('user'),
-  computed: {
-    defaultAvatar() {
-      return defaultAvatar
-    }
-  },
   props: {
     scope: {
       type: Object,
@@ -32,7 +27,7 @@ export default defineComponent({
     const { value } = createRowFieldValues(props)
     const options = createOptions(props, getConfig('user'))
 
-    const avatar = computed(()=>{
+    const avatar = computed(() => {
       return (value.value.avatar === '' || !value.value.avatar) ? defaultAvatar : value.value.avatar
     })
 
@@ -43,27 +38,31 @@ export default defineComponent({
       return rest
     })
 
-    return ()=>(
+    return () => (
       <>
-      <ElPopover
-        placement="right-start"
-        width={280}
-        trigger="click"
-        content="this is content, this is content, this is content"
-        v-slots={{
-          reference:()=>(
-            <ElLink>
-              <ElAvatar src={avatar.value} size={25}/>
-              <span class="ml-4px">{value.value.nickname}</span>
-
-            </ElLink>
-          ),
-          default: () => (<UserInfo user={value.value}></UserInfo>)
-        }}
-      >
-      </ElPopover>
+        <ElPopover
+          placement="right-start"
+          width={280}
+          trigger="click"
+          content="this is content, this is content, this is content"
+          v-slots={{
+            reference: () => (
+              <ElLink underline={false}>
+                <ElAvatar src={avatar.value} size={25} />
+                <span class="ml-4px">{value.value.nickname}</span>
+              </ElLink>
+            ),
+            default: () => (<UserInfo user={value.value}></UserInfo>),
+          }}
+        >
+        </ElPopover>
       </>
     )
+  },
+  computed: {
+    defaultAvatar() {
+      return defaultAvatar
+    },
   },
 })
 </script>

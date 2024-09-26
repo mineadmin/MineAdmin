e72003936358767b787ebd76c0b64b876e8c7595
zds <49744633+zds-s@users.noreply.github.com>
2024-09-26 17:36:47 +0800

    reactor(framework): 统一目录命名规范
    Signed-off-by: zds <49744633+zds-s@users.noreply.github.com>

--- app/Event/User/LogoutEvent.php
+++ app/Event/User/LogoutEvent.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
  */
 
-namespace App\Events\User;
+namespace App\Event\User;
 
 use App\Model\Permission\User;
 

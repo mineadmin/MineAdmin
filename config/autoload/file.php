94510a1c2866e71bd606ca419433a6765b4dc669
X.Mo <root@imoi.cn>
2024-09-30 17:12:29 +0800

    fix(public_url): 错误的问题

--- config/autoload/file.php
+++ config/autoload/file.php
@@ -23,7 +23,7 @@ return [
         'local' => [
             'driver' => LocalAdapterFactory::class,
             'root' => BASE_PATH . '/storage/uploads',
-            'public_url' => env('APP_URL') . '/storage/uploads',
+            'public_url' => env('APP_URL') . '/uploads',
         ],
         'oss' => [
             'driver' => AliyunOssAdapterFactory::class,

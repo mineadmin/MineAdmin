72ce4c73cd24fdd12f2a12066c6238601c701cf1
X.Mo <root@imoi.cn>
2024-10-07 00:51:26 +0800

    feat(增强ma-tree组件)

--- web/src/components/ma-tree/index.vue
+++ web/src/components/ma-tree/index.vue
@@ -12,14 +12,26 @@ en:
   placeholder: Search Label
   open: Open
   fold: Fold
+  strictlyMode: Strictly mode
+  strictlyModeTip: Whether or not to follow a parent-child is not related to each other
+  selectAll: Select All
+  invert: Invert
 zh_CN:
   placeholder: 搜索名称
   open: 展开
   fold: 折叠
+  strictlyMode: 严格模式
+  strictlyModeTip: 是否遵循父子不互相关联
+  selectAll: 全选
+  invert: 反选
 zh_TW:
   placeholder: 檢索名稱
   open: 展開
   fold: 收起
+  strictlyMode: 嚴格模式
+  strictlyModeTip: 是否遵循父子不互相关聯
+  selectAll: 全選
+  invert: 反選
 </i18n>
 
 <script setup lang="ts">
@@ -37,6 +49,7 @@ const t = useLocalTrans()
 
 const filterText = ref<string>('')
 const treeRef = ref<InstanceType<typeof ElTree>>()
+const checkStrictly = ref<boolean>(false)
 
 watch(() => filterText.value, (val) => {
   treeRef.value!.filter(val)
@@ -56,15 +69,47 @@ function toggle(state: boolean) {
   })
 }
 
-defineExpose({ toggle, elTree: treeRef })
+function handleSelectAll(value: boolean) {
+  if (value) {
+    treeRef.value?.store._getAllNodes()?.map((item) => {
+      item.checked = true
+    })
+  }
+  else {
+    treeRef.value?.setCheckedKeys([])
+  }
+}
+
+function handleInvert() {
+  treeRef.value?.store._getAllNodes()?.map((item) => {
+    item.checked = !item.checked
+  })
+}
+
+defineExpose({
+  toggle,
+  handleSelectAll,
+  elTree: treeRef,
+})
 </script>
 
 <template>
   <div class="sticky w-full">
     <div v-if="$attrs?.showCheckbox ?? false" class="flex items-center">
-      <el-checkbox>全选</el-checkbox>
-      <el-checkbox>反选</el-checkbox>
-      <el-checkbox>关闭父子联动</el-checkbox>
+      <el-checkbox @change="v => checkStrictly = v as boolean">
+        <div class="flex items-center gap-x-1">
+          {{ t('strictlyMode') }}
+          <el-tooltip :content="t('strictlyModeTip')">
+            <ma-svg-icon name="material-symbols:info-outline-rounded" :size="16" />
+          </el-tooltip>
+        </div>
+      </el-checkbox>
+      <el-checkbox @change="(value) => handleSelectAll(value as boolean)">
+        {{ t('selectAll') }}
+      </el-checkbox>
+      <el-checkbox v-if="checkStrictly" @change="handleInvert">
+        {{ t('invert') }}
+      </el-checkbox>
     </div>
     <div class="flex items-center justify-between gap-x-1">
       <el-input v-model="filterText" :placeholder="t('placeholder')" clearable>
@@ -83,10 +128,11 @@ defineExpose({ toggle, elTree: treeRef })
     </div>
   </div>
   <ElTree
-    v-bind="$attrs"
     ref="treeRef"
+    :check-strictly="checkStrictly"
     :filter-node-method="filterNode"
     class="w-auto overflow-y-auto"
+    v-bind="$attrs"
   >
     <template #default="{ node, data }">
       <slot v-bind="{ node, data }" />

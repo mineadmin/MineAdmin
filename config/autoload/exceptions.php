71b162e6e28ee409f1138f3919283f6a5706b7eb
zds <49744633+zds-s@users.noreply.github.com>
2024-10-25 16:52:11 +0800

    feat(config): add ModeNotFoundHandler to exception handlers
    - Include ModeNotFoundHandler in the list of exception handlers
    - This handler is responsible for handling mode not found exceptions

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -12,12 +12,14 @@ declare(strict_types=1);
 use App\Exception\Handler\AppExceptionHandler;
 use App\Exception\Handler\BusinessExceptionHandler;
 use App\Exception\Handler\JwtExceptionHandler;
+use App\Exception\Handler\ModeNotFoundHandler;
 use App\Exception\Handler\UnauthorizedExceptionHandler;
 use App\Exception\Handler\ValidationExceptionHandler;
 
 return [
     'handler' => [
         'http' => [
+            ModeNotFoundHandler::class,
             // 处理业务异常
             BusinessExceptionHandler::class,
             // 处理未授权异常

420456c2acfbe840808455061d5e0644b46f01ae
X.Mo <root@imoi.cn>
2025-03-10 10:40:29 +0800

    fix:(data seeder) 修复填充部门菜单数据bug

--- databases/migrations/2025_02_24_195620_create_department_tables.php
+++ databases/migrations/2025_02_24_195620_create_department_tables.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Model\Permission\Menu;
 use Hyperf\Database\Schema\Schema;
 use Hyperf\Database\Schema\Blueprint;
 use Hyperf\Database\Migrations\Migration;
@@ -66,48 +65,6 @@ return new class extends Migration
             $table->datetimes();
             $table->softDeletes();
         });
-        $parent = Menu::where('name','permission')->firstOrFail();
-        $now = Menu::create([
-            'name'  =>  'permission:department',
-            'path'  =>  '/permission/departments',
-            'parent_id' => $parent->id,
-            'meta'  =>  [
-                'title' => '部门管理',
-                'icon' => 'ri:git-repository-private-line',
-                'type' => 'M',
-                'hidden' => 0,
-                'componentPath' => 'modules/',
-                'componentSuffix' => '.vue',
-                'breadcrumbEnable' => 1,
-                'copyright' => 1,
-                'cache' => 1,
-                'affix' => 0,
-            ]
-        ]);
-        $children = [
-            'permission:department:index' => '部门列表',
-            'permission:department:save'    =>  '部门新增',
-            'permission:department:update'  =>  '部门编辑',
-            'permission:department:delete'  =>  '部门删除',
-        ];
-        foreach ($children as $child => $title) {
-            Menu::create([
-                'name'  =>  $child,
-                'path'  =>  '/permission/departments',
-                'meta'  =>  [
-                    'title' => $title,
-                    'icon' => 'ri:git-repository-private-line',
-                    'type' => 'M',
-                    'hidden' => 1,
-                    'componentPath' => 'modules/',
-                    'componentSuffix' => '.vue',
-                    'breadcrumbEnable' => 1,
-                    'cache' => 1,
-                    'affix' => 0,
-                ],
-                'parent_id' => $now->id
-            ]);
-        }
     }
 
     /**

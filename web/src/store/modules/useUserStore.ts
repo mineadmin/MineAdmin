1f4d9de08544abfe661e87de09dba75feecd5ee9
west_ng <457395070@qq.com>
2025-04-12 20:03:11 +0800

    fix：Fix the issue of keeping the user's default language after logging in

--- web/src/store/modules/useUserStore.ts
+++ web/src/store/modules/useUserStore.ts
@@ -114,6 +114,7 @@ const useUserStore = defineStore(
           cache.set('token', res.data.access_token)
           cache.set('expire', useDayjs().unix() + res.data.expire_at, { exp: res.data.expire_at })
           cache.set('refresh_token', res.data.refresh_token)
+          setLanguage(setting.getSettings('app').useLocale ?? 'zh_CN')
           await usePluginStore().callHooks('login', { username: data.username, ...res.data })
           resolve(res.data)
         }).catch((error) => {

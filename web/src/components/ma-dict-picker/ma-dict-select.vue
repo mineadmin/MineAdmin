5e58d1a89284955a7211a349b89e5bdd05a25f01
X.Mo <root@imoi.cn>
2025-04-30 16:33:50 +0800

    fix: 还原 ma-dict-select 组件

--- web/src/components/ma-dict-picker/ma-dict-select.vue
+++ web/src/components/ma-dict-picker/ma-dict-select.vue
@@ -40,16 +40,17 @@ const model = defineModel<any>()
 
 <template>
   <el-select v-model="model" v-bind="$attrs">
-    <template v-for="slot in Object.keys($slots)" #[slot]>
-      <slot :name="slot">
-        <template v-if="dictionaryData && slot === 'default'">
-          <template v-for="item in dictionaryData as Dictionary[]" :key="item">
-            <el-option :value="item.value" :label="item?.i18n ? t(item.i18n) : item.label">
-              <slot v-if="$slots.optionDefault" name="optionDefault" />
-            </el-option>
-          </template>
+    <slot name="default">
+      <template v-if="dictionaryData">
+        <template v-for="item in dictionaryData as Dictionary[]" :key="item">
+          <el-option :value="item.value" :label="item?.i18n ? t(item.i18n) : item.label">
+            <slot v-if="$slots.optionDefault" name="optionDefault" />
+          </el-option>
         </template>
-      </slot>
+      </template>
+    </slot>
+    <template v-for="slot in Object.keys($slots)" #[slot]>
+      <slot v-if="slot !== 'default'" :name="slot" />
     </template>
   </el-select>
 </template>

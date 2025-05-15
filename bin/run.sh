933bdab1f97dcc147856cbe8a71fd49672f9cdc6
X.Mo <root@imoi.cn>
2025-05-15 16:00:52 +0800

    feat: 增加一键启动脚本

--- bin/run.sh
+++ bin/run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # 启动项目
-startProject() {
-
+function startProject() {
+  echo 111
 }
 
 # 停止项目
-stopProject() {
-
+function stopProject() {
+  echo 222
 }
 
 # 重启项目
-restartProject() {
-
+function restartProject() {
+  echo 333
 }
 
 # 显示状态
-showStatus() {
-
+function showStatus() {
+  echo 444
 }
 
 # 帮助
@@ -26,28 +26,28 @@ showHelp() {
 }
 
 case "$1" in
-    start)
-        startProject
-        ;;
-    stop)
-        stopProject
-        ;;
-    restart)
-        restartProject
-        ;;
-    status)
-        showStatus
-        ;;
-    help|--help|-h)
-        showHelp
-        ;;
-    *)
-        if [ -z "$1" ]; then
-            startProject
-        else
-            bilingual "命令不存在: $1" "Unknown command: $1"
-            showHelp
-            exit 1
-        fi
-        ;;
+  start)
+    startProject
+    ;;
+  stop)
+    stopProject
+    ;;
+  restart)
+    restartProject
+    ;;
+  status)
+    showStatus
+    ;;
+  help|--help|-h)
+    showHelp
+    ;;
+  *)
+    if [ -z "$1" ]; then
+      startProject
+    else
+      bilingual "命令不存在: $1" "Unknown command: $1"
+      showHelp
+      exit 1
+    fi
+    ;;
 esac

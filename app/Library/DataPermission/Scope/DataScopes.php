c9f41c533f46c5a3abe05314f61e12820e6886d6
zds <49744633+zds-s@users.noreply.github.com>
2025-05-24 14:42:41 +0800

    feat(DataPermission): 添加数据权限全局作用域
    - 新增 DataScope 类实现 Hyperf\Database\Model\Scope 接口
    - 创建 DataScopes trait 用于在模型中应用数据权限作用域

---
+++ app/Library/DataPermission/Scope/DataScopes.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+
+namespace App\Library\DataPermission\Scope;
+
+use Hyperf\DbConnection\Model\Model;
+
+/**
+ * @mixin Model
+ */
+trait DataScopes
+{
+    public static function bootDataScopes(): void
+    {
+        static::addGlobalScope(DataScope::class);
+    }
+}

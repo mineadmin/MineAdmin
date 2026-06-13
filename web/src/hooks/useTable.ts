fc87dbc1555171c1615e74147964fe2c40316f25
X.Mo <root@imoi.cn>
2026-06-13 22:14:53 +0800

    移除 web 目录

--- web/src/hooks/useTable.ts
+++
@@ -1,6 +0,0 @@
-import { useTable as useMaTable } from '@mineadmin/table'
-import type { MaTableExpose } from '@mineadmin/table'
-
-export default function useTable(refName: string): Promise<MaTableExpose> {
-  return useMaTable(refName)
-}
